8ba8e7a4e35d92633cc39fa76d965ca2c8af7967
nguyen minh khue <ntmkhue.20it6@vku.udn.vn>
2023-12-21 15:57:20 +0700

    change cm

--- movieApp_api/urls.py
+++ movieApp_api/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
     # path('',include('thearter.urls')),
