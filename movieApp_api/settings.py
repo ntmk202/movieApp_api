bfef6d56cac5420c63e386a97efee533fec38e38
nguyen minh khue <ntmkhue.20it6@vku.udn.vn>
2023-12-23 11:46:18 +0700

    change cm

--- movieApp_api/settings.py
+++ movieApp_api/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-if DEBUG: 
-    STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# if DEBUG: 
+#     STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
--- movieApp_api/urls.py
+++ movieApp_api/urls.py
@@ -20,8 +20,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
     # path('',include('thearter.urls')),
